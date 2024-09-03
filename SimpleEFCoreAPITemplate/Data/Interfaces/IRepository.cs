8ce33c916207b7c6ccf306afe494aaa3f31bc15c
Chetan Naithani <naithani.chetan@gmail.com>
2024-09-03 14:39:13 +0530

    Updating repository classes to include Async functions!

--- SimpleEFCoreAPITemplate/Data/Interfaces/IRepository.cs
+++ SimpleEFCoreAPITemplate/Data/Interfaces/IRepository.cs
@@ -8,21 +8,21 @@ namespace SimpleEFCoreAPITemplate.Data.Interfaces
         void Update(T entity);
         void Delete(T entity);
         void Delete(Expression<Func<T, bool>> where);
-        T GetById(long Id);
-        T GetById(string Id);
-        T Get(Expression<Func<T, bool>> where);
+        Task DeleteAsync(Expression<Func<T, bool>> where);
+        T? Get(Expression<Func<T, bool>> where);
+        Task<T?> GetAsync(Expression<Func<T, bool>> where);
+        T? GetById(long Id);
+        Task<T?> GetByIdAsync(long Id);
         IEnumerable<T> GetAll();
+        Task<T> GetAllAsync();
         IQueryable<T> GetAllQuery();
         IEnumerable<T> GetMany(Expression<Func<T, bool>> where);
         IQueryable<T> GetManyQuery(Expression<Func<T, bool>> where);
-        Task AddAsync(T entity);
-        Task UpdateAsync(T entity);
-        Task DeleteAsync(T entity);
-        Task DeleteAsync(Expression<Func<T, bool>> where);
-        Task<T> GetByIdAsync(long Id);
-        Task<T> GetByIdAsync(string Id);
-        Task<T> GetAsync(Expression<Func<T, bool>> where);
-        Task<T> GetAllAsync();
         Task<T> GetManyAsync(Expression<Func<T, bool>> where);
+
+
+
+
+   
     }
 }
--- SimpleEFCoreAPITemplate/Data/RepositoryBase.cs
+++ SimpleEFCoreAPITemplate/Data/RepositoryBase.cs
@@ -37,23 +37,41 @@ namespace SimpleEFCoreAPITemplate.Data
             {
                 dbset.Remove(entity);
             }
-            public virtual void Delete(Expression<Func<T, bool>> where)
+            public void Delete(Expression<Func<T, bool>> where)
             {
                 IEnumerable<T> objects = dbset.Where(where).AsEnumerable();
                 foreach (T obj in objects)
                     dbset.Remove(obj);
             }
-            public virtual T GetById(long id)
+            public async Task DeleteAsync(Expression<Func<T, bool>> where)
+            {
+                IEnumerable<T> objects = await dbset.Where(where).ToListAsync();
+                foreach (T obj in objects)
+                    dbset.Remove(obj);
+            }
+            public virtual T? GetById(long id)
             {
                 return dbset.Find(id);
             }
-            public virtual T GetById(string id)
+            public async Task<T?> GetByIdAsync(long Id)
             {
-                return dbset.Find(id);
+                return await dbset.FindAsync(Id);
+            }
+            public T? Get(Expression<Func<T, bool>> where)
+            {
+                return dbset.FirstOrDefault(where);
+            }
+            public async Task<T?> GetAsync(Expression<Func<T, bool>> where)
+            {
+                return await dbset.Where(where).FirstOrDefaultAsync();
             }
             public virtual IEnumerable<T> GetAll()
             {
                 return dbset.ToList();
+            }
+            public Task<T> GetAllAsync()
+            {
+                return Task.Run(() => GetAllAsync());
             }
             public virtual IQueryable<T> GetAllQuery()
             {
@@ -63,60 +81,15 @@ namespace SimpleEFCoreAPITemplate.Data
             {
                 return dbset.Where(where).ToList();
             }
-            public virtual IQueryable<T> GetManyQuery(Expression<Func<T, bool>> where)
-            {
-                return dbset.Where(where);
-            }
-            public T Get(Expression<Func<T, bool>> where)
-            {
-                return dbset.FirstOrDefault(where);
-            }
-
-            public Task AddAsync(T entity)
-            {
-                return Task.Run(() => Add(entity));
-
-            }
-
-            public Task UpdateAsync(T entity)
-            {
-                return Task.Run(() => Add(entity));
-            }
-
-            public Task DeleteAsync(T entity)
-            {
-                return Task.Run(() => Add(entity));
-            }
-
-            public Task DeleteAsync(Expression<Func<T, bool>> where)
-            {
-                return Task.Run(() => DeleteAsync(where));
-            }
-
-            public Task<T> GetByIdAsync(long Id)
-            {
-                return Task.Run(() => GetByIdAsync(Id));
-            }
-
-            public Task<T> GetByIdAsync(string Id)
-            {
-                return Task.Run(() => GetByIdAsync(Id));
-            }
-
-            public Task<T?> GetAsync(Expression<Func<T, bool>> where)
-            {
-                return dbset.Where(where).FirstOrDefaultAsync();
-            }
-
-            public Task<T> GetAllAsync()
-            {
-                return Task.Run(() => GetAllAsync());
-            }
-
             public Task<T> GetManyAsync(Expression<Func<T, bool>> where)
             {
                 return Task.Run(() => GetManyAsync(where));
             }
+            public virtual IQueryable<T> GetManyQuery(Expression<Func<T, bool>> where)
+            {
+                return dbset.Where(where);
+            }
+
         }
     }
 }
